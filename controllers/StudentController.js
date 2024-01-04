29da4040a9830ff96c8138d2e19117d4549886e2
IlmanNK <ilmanponka345@gmail.com>
2024-01-04 22:42:20 +0700

    Praktikum dan tugas Pertemuan 12

--- controllers/StudentController.js
+++ controllers/StudentController.js
@@ -1,44 +1,76 @@
-// Membuat class StudentController
+const studentData = require('../data/student');
+const Student = require('../models/Student');
+
 class StudentController {
-    index(req, res) {
-        const data = {
-            message: "Menampilkan semua students",
-            data: [],
-        };
-        res.json(data);
+
+    /** 
+     * Mengambil semua data students
+    */
+    async index(req, res) {
+        // get all students
+        const students = await Student.all();
+
+        const student = {
+            message: 'Menampilkan semua students',
+            data: students
+        }
+        return res.json(student);
     }
 
-    store(req, res) {
-        const { nama } = req.body;
-        const data = {
-            message: `Menambahkan data students ${nama}`,
-            data: [],
-        };
-        res.json(data);
+    /** 
+     * Menyimpan data student baru
+    */
+    async store(req, res) {
+        const studentModel = new Student();
+        const students = await studentModel.save(req.body);
+        return res.json({
+            message: `Berhasil membuat student baru`,
+            data: students
+        });
     }
 
-    update(req, res) {
-        const { id } = req.params;
-        const { nama } = req.body;
-        const data = {
-            message: `Mengedit student id ${id}, nama ${nama}`,
-            data: [],
-        };
-        res.json(data);
+    /** 
+     * Menampilkan data student by id
+    */
+    async show(req, res) {
+        const userId = req.params.id;
+        const student = await Student.find(userId);
+        return res.json({
+            message: `Menampilkan student dengan id ${req.params.id}`,
+            data: student
+        });
     }
 
-    destroy(req, res) {
-        const { id } = req.params;
-        const data = {
-            message: `Menghapus students id ${id}`, 
-            data: [],
-        };
-        res.json(data);
+    /** 
+     * Mengubah data student by id
+    */
+    async update(req, res) {
+        const studentModel = new Student();
+        const userId = req.params.id;
+        const result = await studentModel.update(userId, req.body);
+
+
+        return res.json({
+            message: `Mengupdate student dengan id ${req.params.id}`,
+            data: result
+        });
+    }
+
+    /** 
+     * Menghapus data student by id
+    */
+    async destroy(req, res) {
+        const userId = req.params.id;
+        const studentModel = new Student();
+        const result = await studentModel.delete(userId);
+
+        return res.json({
+            message: `Menghapus student dengan id ${req.params.id}`,
+            data: result
+        });
     }
 }
 
-// Membuat object StundetController
 const object = new StudentController();
 
-// Export object StudentController
 module.exports = object;
---
+++ data/student.js
@@ -0,0 +1,39 @@
+const studentData = [
+    {
+        id: 1,
+        name: "Dira",
+        age: 16,
+        grade: "10 SMA",
+        courses: ["Kimia", "Fisika", "Matematika"]
+    },
+    {
+        id: 2,
+        name: "Alsyad",
+        age: 18,
+        grade: "12 SMA",
+        courses: ["Ekonomi", "Geologi", "Sejarah"]
+    },
+    {
+        id: 3,
+        name: "Fajar",
+        age: 17,
+        grade: "12 SMA",
+        courses: ["Sejarah", "Matematika", "Bahasa Inggris"]
+    },
+    {
+        id: 4,
+        name: "Dika Prasetyo",
+        age: 17,
+        grade: "11 SMA",
+        courses: ["Bahasa Inggris", "Kimia", "Biologi"]
+    },
+    {
+        id: 5,
+        name: "Eka Suryadi",
+        age: 18,
+        grade: "12 SMA",
+        courses: ["Geologi", "Sosiologi", "Sejarah"]
+    },
+]
+
+module.exports = studentData;
